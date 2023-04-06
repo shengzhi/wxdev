bf4d856ca4b5671d94310ed4787c08171c0d6eb3
Gong Shengzhi <gongshengzhi@wutongchain.com>
2023-04-06 16:15:29 +0800

    fix issue

--- userinfo.go
+++ userinfo.go
@@ -116,7 +116,7 @@ type LoginAccessToken struct {
 }
 
 func (c *WXClient) GetLoginAccessToken(code string) (LoginAccessToken, error) {
-	const uri = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%sE&grant_type=authorization_code"
+	const uri = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code"
 	var token LoginAccessToken
 	err := c.httpGet(fmt.Sprintf(uri, c.appid, c.appsecret, code), &token)
 	if err != nil {
